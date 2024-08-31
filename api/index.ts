ba290fcbf9ad0a4ef41a8428177746145ed91837
Elbert Jean <elbertjeandossantos@gmail.com>
2024-08-31 11:01:52 -0300

    :sparkles: feat Segundo teste

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/morpheus", (req, res) => res.send("Tamo no a, ihuuuuuuuuul"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
